8b4b63e61319a3d77af0176c4dbc84b4d41fd460
Jerry Sin <jerry.at.crosssea@gmail.com>
2026-06-07 19:32:03 -0400

    update sitemap and redirection

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,45 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/contact-us',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/videos',
+        destination: '/gallery',
+        permanent: true,
+      },
+      {
+        source: '/sitemap',
+        destination: '/sitemap.xml',
+        permanent: true,
+      },
+      {
+        source: '/places-to-fly',
+        destination: '/location',
+        permanent: true,
+      },
+      {
+        source: '/place-to-stay-in-chetwynd',
+        destination: '/location',
+        permanent: true,
+      },
+      {
+        source: '/chetwynd-accommodations',
+        destination: '/location',
+        permanent: true,
+      },
+      {
+        source: '/Users/Account/LogOn',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
